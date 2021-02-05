38d0837ef11b62718d920b92440c975d4ef24dcf
Josh <ltjosh25@gmail.com>
2021-02-05 12:42:53 -0800

    fixed json key checker

--- prefs.py
+++ prefs.py
@@ -9,22 +9,36 @@ async def checkKeys(server):
 		sjson = json.load(s)
 
 	sid = str(server.id)
-	if len(sjson[sid]) < keyNum:
-		wmsgB = sjson[sid]['wmsg']
-		arole1B = sjson[sid]['arole1']
-		arole2B = sjson[sid]['arole2']
-		arole3B = sjson[sid]['arole3']
-		arole4B = sjson[sid]['arole4']
-		arole5B = sjson[sid]['arole5']
-		if 'prefix' in sjson[sid]:
-			prefixB = sjson[sid]['prefix']
-		else:
-			prefixB = "!"
+	try:
+		if len(sjson[sid]) < keyNum:
+			wmsgB = sjson[sid]['wmsg']
+			arole1B = sjson[sid]['arole1']
+			arole2B = sjson[sid]['arole2']
+			arole3B = sjson[sid]['arole3']
+			arole4B = sjson[sid]['arole4']
+			arole5B = sjson[sid]['arole5']
+			if 'prefix' in sjson[sid]:
+				prefixB = sjson[sid]['prefix']
+			else:
+				prefixB = "!"
+			sjson[sid] = {}
+			sjson[sid]['wmsg'] = wmsgB
+			sjson[sid]['arole1'] = arole1B
+			sjson[sid]['arole2'] = arole2B
+			sjson[sid]['arole3'] = arole3B
+			sjson[sid]['arole4'] = arole4B
+			sjson[sid]['arole5'] = arole5B
+			sjson[sid]['prefix'] = prefixB
 
-		del sjson[sid]
-
-		### re-adds the previous attributes, plus new ones
-
+			del sjson[sid]
+	except: # if server not in json
+		wmsgB = ""
+		arole1B = ""
+		arole2B = ""
+		arole3B = ""
+		arole4B = ""
+		arole5B = ""
+		prefixB = "!"
 		sjson[sid] = {}
 		sjson[sid]['wmsg'] = wmsgB
 		sjson[sid]['arole1'] = arole1B
@@ -34,8 +48,8 @@ async def checkKeys(server):
 		sjson[sid]['arole5'] = arole5B
 		sjson[sid]['prefix'] = prefixB
 
-		with open('serverprefs.json', 'w') as s:
-			json.dump(sjson, s)
+	with open('serverprefs.json', 'w') as s:
+		json.dump(sjson, s)
 
 async def addServerIfNeeded(sjson, server):
 	sid = int(server.id)
